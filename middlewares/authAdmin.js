9511119e6b41e64a386ccc915eded0d8432ee02a
Viral Patel <pviral3011@gmail.com>
2025-03-15 11:33:10 +0530

    feat: implemented doctor creation using controller and model

---
+++ middlewares/authAdmin.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+
+// Admin authentication middleware
+
+const authAdmin = async (req, res, next) => {
+  try {
+    const { atoken } = req.headers;
+    if (!atoken) {
+      return res.json({
+        success: false,
+        message: "Not Authorized! Please Login again.",
+      });
+    }
+
+    const token_decode = jwt.verify(atoken, process.env.JWT_SECRET);
+    if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+      return res.json({
+        success: false,
+        message: "Not Authorized! Please Login again.",
+      });
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message || "Error occured during doctor creation",
+    });
+  }
+};
+
+export default authAdmin;
