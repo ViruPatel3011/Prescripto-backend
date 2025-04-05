e128715c5f174e7eaab6c1041374c98ad1447a62
Viral Patel <pviral3011@gmail.com>
2025-04-05 13:51:27 +0530

    feat: completed doctor panel apis

--- routes/doctorRoute.js
+++ routes/doctorRoute.js
@@ -1,8 +1,26 @@
 import express from "express";
-import { getAllDoctorsList } from "../controllers/doctorController.js";
+import {
+  getAllDoctorsList,
+  loginDoctor,
+  getDoctorAppointments,
+  appointmentComplete,
+  appointmentCancel,
+  doctorDashboard,
+  getDoctorProfile,
+  updateDoctorProfile,
+} from "../controllers/doctorController.js";
+import authDoctor from "../middlewares/authDoctor.js";
 
 const doctorRouter = express.Router();
 
 doctorRouter.get("/list", getAllDoctorsList);
+doctorRouter.post("/login", loginDoctor);
+doctorRouter.get("/appointments", authDoctor, getDoctorAppointments);
+doctorRouter.post("/complete-appointment", authDoctor, appointmentComplete);
+doctorRouter.post("/cancel-appointment", authDoctor, appointmentCancel);
+doctorRouter.get("/doctor-dashboard", authDoctor, doctorDashboard);
+doctorRouter.get("/doctor-dashboard", authDoctor, getDoctorProfile);
+doctorRouter.get("/profile", authDoctor, getDoctorProfile);
+doctorRouter.post("/update-profile", authDoctor, updateDoctorProfile);
 
 export default doctorRouter;
